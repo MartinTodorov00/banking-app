fcc6f4065921b34801ca52e8c991aa5c2cfbce57
Marto <martintodorovb@gmail.com>
2022-02-11 09:07:28 +0200

    tasks update

--- Banking app/app/src/main/java/run/Main.java
+++ Banking app/app/src/main/java/run/Main.java
@@ -11,11 +11,15 @@ public class Main {
         //Tasks:
         //Update for users(v nastroiki da se napravi pole za promqna na imeto) // Database and Swing // wait swing
         //Update for password(v nastroiki da se napravi pole za promqna na parolata na usera) // Database and Swing(wait swing)
-        //login na user problem // Fixed!
+
         //Problem s akauntite(pri natiskane na izhod i logvane na drug user, se pokazvat dannite na purviq lognat user) // Swing
-        //Prevod po smetka // ne se znae
+        //opravqne na scaling // Swing
+
         //Promqna na cvetovete na prilojenieto za da e po krasivo // Swing
         //Promqna na kartinkata na prilojenieto // Swing
-        //opravqne na scaling // Swing
+
+
+        //login na user problem // Fixed!
+        //Prevod po smetka// Fixed!
     }
 }
