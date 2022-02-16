c0f687c6af127441e5fbdf5edd598c7489d3e94b
Marto <martintodorovb@gmail.com>
2022-02-16 14:17:40 +0200

    Tasks updated

--- Banking app/app/src/main/java/run/Main.java
+++ Banking app/app/src/main/java/run/Main.java
@@ -9,12 +9,11 @@ public class Main {
         FrontEndControl.run();
 
         //Tasks:
+        //search by two criteria - two times // BackEnd and Swing
+
         //Problem s akauntite(pri natiskane na izhod i logvane na drug user, se pokazvat dannite na purviq lognat user) // Swing
         //Prevod po smetka // moje bi zaradi gorniq problem
 
-        //search by two criteria - two times // BackEnd and Swing
-
         //opravqne na scaling // Swing
-        //Promqna na cvetovete na prilojenieto za da e po krasivo // Swing
     }
 }
